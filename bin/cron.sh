76094bb675e75dfedb343c099ecbd49b710ebebe
Graylin Kim <graylin.kim@gmail.com>
2013-11-13 14:33:19 -0500

    Add a shell script for cronjobs.

---
+++ bin/cron.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source $(dirname "$0")/utils.sh
+
+# This should run once a day at the time we'd like digests to go out.
+$ROOTDIR/bin/run.sh UpdateSenators -e $ROOTDIR/target/BillBuzz2-$VERSION/WEB-INF/classes/app.properties
+$ROOTDIR/bin/run.sh UpdatePosts -e $ROOTDIR/target/BillBuzz2-$VERSION/WEB-INF/classes/app.properties
+$ROOTDIR/bin/run.sh SendDigests -e $ROOTDIR/target/BillBuzz2-$VERSION/WEB-INF/classes/app.properties
+
---
+++ bin/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source $(dirname "$0")/utils.sh
+
+BASE="$ROOTDIR/target/BillBuzz2-$VERSION/WEB-INF"
+SCRIPT=$1; shift
+
+if [ ! $SCRIPT ]; then
+    echo "Script name is a required argument.";
+fi
+
+# TODO: This memory size should be an adjustable parameter
+java -Xmx1024m -Xms16m -cp $BASE/classes/:$BASE/lib/* gov.nysenate.billbuzz.scripts.$SCRIPT $@
+
