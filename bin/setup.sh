60c2ae76861517cdbb0a019db559cb05a4241de8
Graylin Kim <graylin.kim@gmail.com>
2014-01-14 16:55:43 -0500

    Fix setup script.

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,9 +3,5 @@ source $(dirname "$0")/utils.sh
 
 lib_dir=$ROOTDIR/lib
 
-function mvn_install {
-    mvn install:install-file -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dfile=$4 -Dpackaging=jar -DgeneratePom=true
-}
-
-mvn_install gov.nysenate.services nysenate-java-client 1.0 $lib_dir/nysenate-java-client-1.0.jar
+mvn install:install-file -Dfile=$lib_dir/nysenate-java-utils-1.0.0.jar -DpomFile=$lib_dir/nysenate-java-utils-1.0.0.pom
 
