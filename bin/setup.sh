f1b96500f1fd5b44a04e1e9df0d858a0e51d17c1
Ken Zalewski <zalewski.senate@gmail.com>
2014-07-11 12:12:05 -0400

    setup.sh is deprecated, since nysenate-java-utils is now retrieved via NYSenate Maven repo.

--- bin/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-script_dir=`dirname $0`
-root_dir=`cd "$script_dir"/..; echo $PWD`
-lib_dir="$root_dir/lib"
-
-mvn install:install-file -Dfile=$lib_dir/nysenate-java-utils-1.0.0.jar -DpomFile=$lib_dir/nysenate-java-utils-1.0.0.pom
-
