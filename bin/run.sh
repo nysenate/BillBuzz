127505717a0b40b7f66206af8e81be601669dc68
Ken Zalewski <zalewski.senate@gmail.com>
2014-10-14 11:55:33 -0400

    Fixed runtime classpath, and bumped Tomcat version.

--- bin/run.sh
+++ bin/run.sh
@@ -3,6 +3,7 @@
 prog=`basename $0`
 script_dir=`dirname $0`
 root_dir=`cd "$script_dir"/..; echo $PWD`
+tomcat_home=/usr/share/tomcat
 pom_file="$root_dir/pom.xml"
 
 if [ ! -r "$pom_file" ]; then
@@ -11,7 +12,7 @@ if [ ! -r "$pom_file" ]; then
 fi
 
 app_ver=`php -r '$x=simplexml_load_file($argv[1]); echo $x->version;' $pom_file`
-base_dir="$root_dir/target/BillBuzz##$app_ver/WEB-INF"
+base_dir="$root_dir/target/billbuzz##$app_ver/WEB-INF"
 
 SCRIPT=$1
 shift
@@ -21,5 +22,5 @@ if [ ! "$SCRIPT" ]; then
   exit 1
 fi
 
-java -Xmx1024m -Xms16m -cp "$base_dir/classes/:$base_dir/lib/*" gov.nysenate.billbuzz.scripts.$SCRIPT -e $base_dir/classes/app.properties $@
+java -Xmx1024m -Xms16m -cp "$base_dir/classes/:$base_dir/lib/*:$tomcat_home/lib/*" gov.nysenate.billbuzz.scripts.$SCRIPT -e $base_dir/classes/app.properties $@
 
